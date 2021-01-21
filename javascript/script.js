f19999b2fb20854eafbe007c5267666c96fbc237
Madhuri <madhuriteli07@gmail.com>
2021-01-21 10:57:38 +0530

    history Pop Up

--- javascript/script.js
+++ javascript/script.js
@@ -1,49 +1,69 @@
 $(document).ready(function () {
-
-    $(".b1").click(function () {
-        $(".th1").css({
-            'background-color': '#BFBEC2',
-            'opacity': '100%'
-        });
+  $(".b1").click(function () {
+    $(".th1").css({
+      "background-color": "#BFBEC2",
+      opacity: "100%",
     });
+  });
 
-    //alert("hello");
-    /*
+  //alert("hello");
+  /*
          $("#case").on("click", function () {
                 $("#table-1").show();
         }); */
 
-    //  $("#case").click(function () {
-    //   $("#table-1").show();
-    // });
+  //  $("#case").click(function () {
+  //   $("#table-1").show();
+  // });
 
-    $('.i1').click(function () {
-        $('.drop').toggle();
-    });
+  $(".i1").click(function () {
+    $(".drop").toggle();
+  });
 
-    $("#table2-toggler").click(function () {
-        $('.table2').toggle();
-    });
+  $("#table2-toggler").click(function () {
+    $(".table2").toggle();
+  });
 
-    //////////pop window//////
-    $("#openForm1").click(function () {
-        $("#caseForm").show();
+  //////////new case pop window//////
+  $("#openForm1").click(function () {
+    $("#caseForm").show();
+  });
 
-    });
+  $(".close").on("click", function () {
+    $("#caseForm").hide();
+  });
 
-    $(".close").on("click", function () {
-        $("#caseForm").hide();
-    });
+  $("#violationCode").click(function () {
+    $("#violationCodeTable").toggle();
+  });
 
+  $("#feeCode").click(function () {
+    $("#feeCodeTable").toggle();
+  });
 
-    $("#violationCode").click(function () {
-        $('#violationCodeTable').toggle();
-    });
+  $("#historyFormOpen").click(function () {
+    $("#historyWindow").show();
+  });
 
-    $("#feeCode").click(function () {
-        $('#feeCodeTable').toggle();
-    });
+  $("#cancelButton").on("click", function () {
+    $("#historyWindow").hide();
+  });
+  /////////////////history//////////////
 
-
-
+  /*
+    
+            var tab = document.getElementsByClassName("history");
+        var i;
+for (i = 0; i < tab.length; i++) {
+  tab[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+*/
 });
